50345c83c5a957756431485faca5d1ef0c91f7ac
Maisie Campbell <maisie-campbell@hotmail.com>
2017-09-20 16:20:00 +0100

    fixed typo in AccountsUserManager class

--- accounts/models.py
+++ accounts/models.py
@@ -8,24 +8,24 @@ from django.utils import timezone
 
 # Create your models here.
 class AccountUserManager(UserManager):
-	def create_user(self, username, email, password,
-					is_staff, is_superuser, **extra_fields)
-	"""
-	Create and saves a user with the given username, email and password.
-	"""
-		now = timezone.now()
-		if not email:
-			raise ValueError('The given username must be set')
-
-		email = self.normalize_email(email)
-		user = self.model(username = email, email = email,
-						  is_staff=is_staff, is_active=True,
-						  is_superuser=is_superuser,
-						  date_joined=now, **extra_fields)
-		user.set_password(password)
-		user.save(using=self.db)
-
-		return user
+    def _create_user(self, username, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """
+       Creates and saves a User with the given username, email and password.
+       """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given username must be set')
+ 
+        email = self.normalize_email(email)
+        user = self.model(username=email, email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+ 
+        return user
 
 class User(AbstractUser):
 
