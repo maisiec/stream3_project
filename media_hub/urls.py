e04d5a344db2da54af8246243da19603bda91f67
Maisie Campbell <maisie-campbell@hotmail.com>
2017-09-20 16:54:27 +0100

    added register url

--- media_hub/urls.py
+++ media_hub/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from home import views
+from accounts import views as accounts_views
+from home import views as home_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.get_index, name='index'),
+    url(r'^$', home_views.get_index, name='index'),
+    url(r'^$', accounts_views.register, name='register'),
 ]
