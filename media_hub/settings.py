8302eae518a89a1b67b4887f4dfb65de10e63780
Maisie Campbell <maisie-campbell@hotmail.com>
2017-09-20 16:09:55 +0100

    added accounts to installed apps and defined auth_user_model

--- media_hub/settings.py
+++ media_hub/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
