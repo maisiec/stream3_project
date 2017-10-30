6e1206cd1c4537235cf26cf516db4c5e06c595b9
Maisie Campbell <maisie-campbell@hotmail.com>
2017-10-30 17:06:06 +0000

    changed currency to GBP from USD

--- products/models.py
+++ products/models.py
@@ -15,19 +15,19 @@ class Product(models.Model):
 
     @property
     def paypal_form(self):
-    	paypal_dict = {
-	    	"business": settings.PAYPAL_RECEIVER_EMAIL,
-	    	"amount": self.price,
-	    	"currency": "GBP",
-	    	"item_name": self.name,
-	    	"invoice": "%s-%s" % (self.pk, uuid.uuid4()),
-	    	"notify_url": settings.PAYPAL_NOTIFY_URL,
-	    	"return_url": "%s/paypal-return" % settings.SITE_URL,
-	    	"cancel_return": "%s/paypal-cancel" % settings.SITE_URL
-	    }
-
-    	return PayPalPaymentsForm(inital=paypal_dict)
-
+        paypal_dict = {
+            "business": settings.PAYPAL_RECEIVER_EMAIL,
+            "amount": self.price,
+            "currency_code": "GBP",
+            "item_name": self.name,
+            "invoice": "%s-%s" % (self.pk, uuid.uuid4()),
+            "notify_url": settings.PAYPAL_NOTIFY_URL,
+            "return_url": "%s/paypal-return" % settings.SITE_URL,
+            "cancel_return": "%s/paypal-cancel" % settings.SITE_URL
+        }
+ 
+        return PayPalPaymentsForm(initial=paypal_dict)
+ 
     def __unicode__(self):
-    	return self.name
+        return self.name
 
