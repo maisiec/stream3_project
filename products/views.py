b4323326b7d76a3ae7b92be900da3cb1b46bf059
Maisie Campbell <maisie-campbell@hotmail.com>
2017-11-08 11:43:29 +0000

    redefined product_detail view

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Product
+from cart.forms import CartAddProductForm
 
 
 # Create your views here.
@@ -11,9 +12,14 @@ def all_products(request):
 	return render(request, "products/products.html", {"products":products})
 
 # View each product individually
-def product_detail(request, id):
-	product_detail = get_object_or_404(Product, pk=id)
-	return render(request, "products/product_details.html",{"product_detail":product_detail})
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug)
+    cart_product_form = CartAddProductForm()
+    return render(request,
+    			"products/detail.html",
+    			{"product": product,
+                 "cart_product_form": cart_product_form})
+                   
 
 def download_page(request):
 	''' The page that you arrive back to from a succesful purchase'''
