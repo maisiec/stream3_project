d15e85efc02af5210e600fc9b32c960f8f57162d
Maisie Campbell <maisie-campbell@hotmail.com>
2017-11-08 11:42:47 +0000

    added slug field for nice looking urls

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.conf import settings
 from django_extensions.db.fields import AutoSlugField
 from paypal.standard.forms import PayPalPaymentsForm
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 # class ProductTag(models.Model):
@@ -17,18 +18,26 @@ from paypal.standard.forms import PayPalPaymentsForm
 #     product = models.ForeignKey("Product")
 
 
-
-
 class Product(models.Model):
 
     '''
     Product.objects.filter(producttagitem__set__tag='paris')
     '''
-    
-    name = models.CharField(max_length=254, default='')
+    name = models.CharField(max_length=254, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     video = models.FileField(upload_to='product_videos')
+
+
+    class Meta:
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.id, self.slug])
 
     @property
     def paypal_form(self):
