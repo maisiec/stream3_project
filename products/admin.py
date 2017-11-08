3a5708ccf6394fdcb997cee76c70139076059624
Maisie Campbell <maisie-campbell@hotmail.com>
2017-11-08 11:45:21 +0000

    added ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from models import Product
 
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price']
+    list_editable = ['price']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product, ProductAdmin)
