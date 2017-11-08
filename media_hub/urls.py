2fe307195f98a3776f3b01871c6b608ebfbcd160
Maisie Campbell <maisie-campbell@hotmail.com>
2017-11-08 17:45:34 +0000

    added table styling

--- media_hub/urls.py
+++ media_hub/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.static import serve
 from .settings import MEDIA_ROOT
@@ -30,6 +30,7 @@ urlpatterns = [
     url(r'^cart/', include('cart.urls', namespace='cart')),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
     url(r'^$', views.get_index),
+    
 
     #Account URLS
     url(r'^pages/', include('django.contrib.flatpages.urls')),
