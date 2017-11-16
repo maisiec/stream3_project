a57c1658badb7cf3414f9a5d4b147af4fbee12fc
Maisie Campbell <maisie-campbell@hotmail.com>
2017-11-16 14:51:12 +0000

    removed paypal info

--- products/models.py
+++ products/models.py
@@ -39,24 +39,8 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.id, self.slug])
 
-    @property
-    def paypal_form(self):
-        paypal_dict = {
-            "business": settings.PAYPAL_RECEIVER_EMAIL,
-            "amount": self.price,
-            "currency_code": "GBP",
-            "item_name": self.name,
-            "invoice": "%s-%s" % (self.pk, uuid.uuid4()),
-            "notify_url": settings.PAYPAL_NOTIFY_URL,
-            "return_url": "%s/paypal-return" % settings.SITE_URL,
-            "cancel_return": "%s/paypal-cancel" % settings.SITE_URL
-        }
- 
-        return PayPalPaymentsForm(initial=paypal_dict)
- 
     def __unicode__(self):
         return self.name
-
 
 
 
