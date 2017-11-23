01a817efcd06261c2b3f142037067ae8540527f6
Maisie Campbell <maisie-campbell@hotmail.com>
2017-11-23 13:06:40 +0000

    added order detail and list urls

--- orders/urls.py
+++ orders/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^create/$', views.order_create, name='order_create'),
+	url(r'^(?P<id>\d+)/$', views.order_detail, name='detail'),
+	url(r'^$', views.order_list, name='list'),
 ]
