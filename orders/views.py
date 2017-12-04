c637d159905249dfa317bac674888c65c526e682
Maisie Campbell <maisie-campbell@hotmail.com>
2017-12-04 13:35:47 +0000

    fixed issue where form wasn't saving

--- orders/views.py
+++ orders/views.py
@@ -15,7 +15,9 @@ def order_create(request):
     if request.method == 'POST':
         form = OrderCreateForm(request.POST)
         if form.is_valid():
-            order = form.save()
+            order = form.save(commit=False)
+            order.user = request.user
+            order.save()
             for item in cart:
                 OrderItem.objects.create(order=order,
                                          product=item['product'],
