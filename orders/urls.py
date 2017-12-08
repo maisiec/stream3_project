cd85a1f3599ae689cf79caea9cf6dfc42484daf1
Maisie Campbell <maisie-campbell@hotmail.com>
2017-12-08 14:08:31 +0000

    moved download url into orders urls

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	url(r'^create/$', views.order_create, name='order_create'),
 	url(r'^(?P<id>\d+)/$', views.order_detail, name='detail'),
 	url(r'^$', views.order_list, name='list'),
+	url(r'^download/(?P<order_id>\d+)/(?P<product_id>\d+)/$', views.download, name='download'), # view a purchase
 ]
