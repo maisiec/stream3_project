df3b332bf7374379a0ca69d3e5b2b423983eb598
Maisie Campbell <maisie-campbell@hotmail.com>
2017-12-10 17:45:09 +0000

    added a CustomUserTest to test registering a user

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,5 +2,50 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import User
+from .forms import UserRegistrationForm
+from django import forms
+from django.conf import settings
 
-# Create your tests here.
+class CustomUserTest(TestCase):
+ 
+    def test_manager_create(self):
+        user = User.objects._create_user(None, "test@test.com",
+                                         "password",
+                                         False, False)
+        self.assertIsNotNone(user)
+ 
+        with self.assertRaises(ValueError):
+            user = User.objects._create_user(None, None, "password",
+                                             False, False)
+
+    def test_registration_form(self):
+        form = UserRegistrationForm({
+            'email': 'test@test.com',
+            'password1': 'letmein1',
+            'password2': 'letmein1',
+        })
+ 
+        self.assertTrue(form.is_valid())
+
+    def test_registration_form_fails_with_missing_email(self):
+        form = UserRegistrationForm({
+            'password1': 'letmein1',
+            'password2': 'letmein1'
+        })
+ 
+        self.assertFalse(form.is_valid())
+        self.assertRaisesMessage(forms.ValidationError,
+                                 "Please enter your email address",
+                                 form.full_clean())
+
+    def test_registration_form_fails_wih_passwords_that_dont_match(self):
+        form = UserRegistrationForm({
+            'email': 'test@test.com',
+            'password1': 'letmein1',
+            'password2': 'letmein3'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertRaisesMessage(forms.ValidationError,
+                                 "Passwords do not match",
+                                 form.full_clean())
