347721a03afbb4dc641e3e8efa3f2517db99bf15
Maisie Campbell <maisie-campbell@hotmail.com>
2017-12-10 22:29:59 +0000

    setting.staging

--- media_hub/wsgi.py
+++ media_hub/wsgi.py
@@ -12,7 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "media_hub.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "media_hub.settings.staging")
+
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(get_wsgi_application())
