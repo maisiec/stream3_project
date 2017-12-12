9ed9d8523f026d39213e7a4d875747514d0e1d34
Maisie Campbell <maisie-campbell@hotmail.com>
2017-12-12 22:46:53 +0000

    added new class to contact form for styling

--- contact/forms.py
+++ contact/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 
 class ContactForm(forms.Form):
-	full_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Full Name'}))
-	email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email'}))
+	full_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-style','placeholder':'Full Name'}))
+	email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class':'form-style','placeholder':'Email'}))
 	content = forms.CharField(
 		required=True,
 		widget=forms.Textarea
-	)
+	(attrs={'class':'form-style','placeholder':'Enter your message here'}))
 
 	def __init__(self, *args, **kwargs):
 		super(ContactForm, self).__init__(*args, **kwargs)
