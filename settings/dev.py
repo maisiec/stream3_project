f6894fe0872868a912ca2e310615fcff044275a7
Maisie Campbell <maisie-campbell@hotmail.com>
2017-12-12 19:09:36 +0000

    Fixing deployment

---
+++ settings/dev.py
@@ -0,0 +1,24 @@
+from settings.base import *
+
+DEBUG = True 
+
+INSTALLED_APPS.append('debug_toolbar')
+
+MIDDLEWARE_CLASSES.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# PayPal Settings
+RESOURCES_DIR = 'media/product_videos/'
+SITE_URL = 'http://127.0.0.1:8000'
+PAYPAL_NOTIFY_URL = 'https://d8d947ba.ngrok.io/a-very-hard-to-guess-url/'
+PAYPAL_RECEIVER_EMAIL = 'maaisiexx-facilitator@hotmail.co.uk'
+PAYPAL_TEST = True
---
+++ settings/staging.py
@@ -0,0 +1,46 @@
+from base import *
+import os
+import dj_database_url
+
+DEBUG = False 
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+
+# Load the ClearDB connection details from the environment variable
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+# Paypal environment variables
+PAYPAL_NOTIFY_URL = 'https://d8d947ba.ngrok.io/a-very-hard-to-guess-url/'
+PAYPAL_RECEIVER_EMAIL = 'maaisiexx-facilitator@hotmail.co.uk'
+ 
+SITE_URL = 'http://wayv-videos.herokuapp.com/'
+ALLOWED_HOSTS.append('wayv-videos.herokuapp.com/')
+ 
+# Log DEBUG information to the console
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'maaisiexx@gmail.com'
+EMAIL_HOST_PASSWORD = 'qpwoeiruTY1'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
