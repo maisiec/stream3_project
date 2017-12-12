e28d11ee482f1716d58ac40cbb6e0b283f68ee10
Maisie Campbell <maisie-campbell@hotmail.com>
2017-12-12 21:23:44 +0000

    removed staticfiles directory

--- staticfiles/js/nav.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#nav-toggle a").click(function(event){
-    $("#main-nav").toggleClass("active");
-    $("#nav-toggle a").toggleClass("active");
-    event.preventDefault();
-  });
-}); 
-
