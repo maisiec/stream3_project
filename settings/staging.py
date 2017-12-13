0ae54ee2e04d3c3e4b8d0cba775763e5aec986c3
Maisie Campbell <maisie-campbell@hotmail.com>
2017-12-13 21:31:31 +0000

    Added env vars

--- settings/staging.py
+++ settings/staging.py
@@ -45,6 +45,9 @@ LOGGING = {
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
 AWS_STORAGE_BUCKET_NAME = "wayv-production"
 AWS_AUTO_CREATE_BUCKET = False
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
